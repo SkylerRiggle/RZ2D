f0fb804754f4b8da92a1bfe95dcfa6a7a2b9b080
skyler.riggle <Skyler.D.Riggle-1@ou.edu>
2023-12-19 16:31:28 -0600

    Component Method Definitions

--- Razor2D/src/ecs/ComponentManager.h
+++ Razor2D/src/ecs/ComponentManager.h
@@ -8,5 +8,20 @@ namespace RZ
     {
     public:
         void DestroyEntity(EntityId entity);
+
+        template <typename T>
+        void AddComponent(EntityId entity, T componentData);
+
+        template <typename T>
+        void ShareComponent(EntityId originalEntity, EntityId newEntity);
+
+        template <typename T>
+        void RemoveComponent(EntityId entity);
+
+        template <typename T>
+        bool HasComponent(EntityId entity);
+
+        template <typename T>
+        T& GetComponent(EntityId entity);
     };
 }
--- Razor2D/src/ecs/ECSCoordinator.h
+++ Razor2D/src/ecs/ECSCoordinator.h
@@ -20,6 +20,36 @@ namespace RZ
 
         // COMPONENT METHODS //////////////////////////////////////////////////
 
+        template <typename T>
+        inline void AddComponent(EntityId entity, T componentData) 
+        { 
+            m_componentManager->AddComponent<T>(entity, componentData); 
+        }
+
+        template <typename T>
+        inline void ShareComponent(EntityId originalEntity, EntityId newEntity)
+        {
+            m_componentManager->ShareComponent<T>(originalEntity, newEntity);
+        }
+
+        template <typename T>
+        inline void RemoveComponent(EntityId entity)
+        {
+            m_componentManager->RemoveComponent<T>(entity);
+        }
+
+        template <typename T>
+        inline bool HasComponent(EntityId entity)
+        {
+            return m_componentManager->HasComponent<T>(entity);
+        }
+
+        template <typename T>
+        inline T& GetComponent(EntityId entity)
+        {
+            return m_componentManager->GetComponent<T>(entity);
+        }
+
     private:
         EntityManager* m_entityManager;
         ComponentManager* m_componentManager;
