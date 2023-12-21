2ed5b9dc51e4a6b958739580c57b2c02ed8f7128
Skyler Riggle <skyler32702@gmail.com>
2023-12-20 20:56:27 -0600

    Logical Reordering of Type Declarations

--- Razor2D/src/core/Types.h
+++ Razor2D/src/core/Types.h
@@ -2,17 +2,19 @@
 
 #include <stdint.h>
 
-#ifndef MAX_ENTITIES
-    #define MAX_ENTITIES 1000
-#endif
-
 using EntityId = uint32_t;
 using ComponentId = uint8_t;
 
-constexpr EntityId NULL_ENTITY = -1;
+constexpr EntityId NULL_ENTITY = EntityId(-1);
+
+#ifndef MAX_ENTITIES
+    #define MAX_ENTITIES 1000
+#else 
 
 #if (MAX_ENTITIES == 4294967295U)
     #define MAX_ENTITIES 4294967294U
+#endif
+
 #endif
 
 namespace RZ
