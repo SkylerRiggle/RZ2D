26cb9d9e001de95d42f5b79d884713262cfee7d3
skyler.riggle <Skyler.D.Riggle-1@ou.edu>
2024-01-09 16:37:54 -0600

    Removed Old ECS Code

--- Razor2D/src/core/Types.h
+++ Razor2D/src/core/Types.h
@@ -1,18 +1,3 @@
 #pragma once
 
-#include <stdint.h>
-
-using Entity = uint32_t;
-using ComponentId = uint16_t;
-using ArchetypeId = uint16_t;
-
-namespace RZ
-{
-    static ComponentId s_componentCounter;
-    template <typename T>
-    static ComponentId GetComponentId()
-    {
-        static ComponentId id = s_componentCounter++;
-        return id;
-    }
-}
+using EntityID = unsigned int;
